3277dc1da0f12b4495ed73c645e413085858bece
paolomandica <paolomandica.p@gmail.com>
2019-05-27 20:18:26 +0200

    Aggiunti i package model e service. Creata la classe Team. Aggiunte righe utili al gitignore.

---
+++ src/main/java/com/jpl/teamx/model/Team.java
@@ -0,0 +1,71 @@
+package com.jpl.teamx.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Teams")
+public class Team {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long id;
+	
+	@ManyToOne
+	@Column
+	private User admin;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column
+	private String location;
+	
+	public Team(User admin, String name, String description, String location) {
+		this.admin = admin;
+		this.name = name;
+		this.description = description;
+		this.location = location;
+	}
+
+	public User getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(User admin) {
+		this.admin = admin;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
