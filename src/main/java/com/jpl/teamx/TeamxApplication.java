804bc057de311423ce8decd62758e134cb3b716c
Giulia Lanzillotta <lanzillottagiulia97@gmail.com>
2019-05-27 09:20:03 +0200

    Initial commit (+ Gradle)

---
+++ src/main/java/com/jpl/teamx/TeamxApplication.java
@@ -0,0 +1,13 @@
+package com.jpl.teamx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamxApplication.class, args);
+	}
+
+}
