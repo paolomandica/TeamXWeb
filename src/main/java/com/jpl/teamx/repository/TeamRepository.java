0661f682527532a4c67d8fe2f3ba336cd9f7ec72
paolomandica <paolomandica.p@gmail.com>
2019-05-27 20:28:15 +0200

    Create ma non implementare service e repo di Team.

---
+++ src/main/java/com/jpl/teamx/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package com.jpl.teamx.repository;
+
+import com.jpl.teamx.model.Team;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    
+}
---
+++ src/main/java/com/jpl/teamx/service/TeamService.java
@@ -0,0 +1,5 @@
+package com.jpl.teamx.service;
+
+public class TeamService {
+    
+}
