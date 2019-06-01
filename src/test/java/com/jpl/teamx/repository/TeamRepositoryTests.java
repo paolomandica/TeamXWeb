f10ed1d8c360bb8fc06a020e40593111e68c9b2c
paolomandica <paolomandica.p@gmail.com>
2019-06-01 17:56:13 +0200

    implementato test altro metodo + nuovo file properties esclusivo per i test

--- src/test/java/com/jpl/teamx/repository/TeamRepositoryTests.java
+++ src/test/java/com/jpl/teamx/repository/TeamRepositoryTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import com.jpl.teamx.model.Team;
 import com.jpl.teamx.model.User;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +20,28 @@ public class TeamRepositoryTests {
     @Autowired
     private TeamRepository teamRepository;
 
+    private User admin;
+    private Team team;
+
+    @Before
+    public void setUp() {
+        admin = new User("paolo","paolo@prova.com","testurl","imageurl");
+        team = new Team(admin,"prova","prova desc", "prova loc");
+        teamRepository.save(team);
+    }
+
     @Test
     public void saveOneTeam() {
 
-        User admin = new User("paolo","paolo@prova.com","testurl","imageurl");
-        Team team = new Team(admin,"prova nome","prova desc", "prova loc");
-
-        teamRepository.save(team);
         Team found = teamRepository.findById(team.getId()).get();
+
+        assertEquals(found.getId(), team.getId());
+    }
+
+    @Test
+    public void findTeamByName() {
+
+        Team found = teamRepository.findByName(team.getName());
 
         assertEquals(found.getId(), team.getId());
     }
