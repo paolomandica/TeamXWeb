0f3a6ebdf44650cbd57a5aecd1e955fa79541313
paolomandica <paolomandica.p@gmail.com>
2019-06-03 21:05:11 +0200

    Aggiunta dependency per junit 4 nel file build.gradle. Implementato ulteriore test su TeamRepo. Testato PostgresSql e funziona.

--- src/main/java/com/jpl/teamx/repository/TeamRepository.java
+++ src/main/java/com/jpl/teamx/repository/TeamRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
     Team findByName(String name);
+
     @Override
     List<Team> findAll();
 
