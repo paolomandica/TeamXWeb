67c6d633fc1b36fd3757524d39505dcaa7cc1add
paolomandica <paolomandica.p@gmail.com>
2019-06-05 09:57:45 +0200

    Ulteriori integrazioni per il database

--- src/main/java/com/jpl/teamx/controller/TeamXController.java
+++ src/main/java/com/jpl/teamx/controller/TeamXController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.aspectj.weaver.ast.Var;
-import org.h2.store.PageInputStream;
+//import org.h2.store.PageInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
