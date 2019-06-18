8bb0f8f1141ab68a466f50808ebe9e28908df02e
luigisigillo30@gmail.com <luigisigillo30@gmail.com>
2019-06-18 20:10:08 +0200

    modificata teamform e error page

--- src/main/java/com/jpl/teamx/controller/TeamXController.java
+++ src/main/java/com/jpl/teamx/controller/TeamXController.java
@@ -7,8 +7,10 @@ import com.jpl.teamx.service.ImageStorageService;
 import com.jpl.teamx.service.TeamService;
 import com.jpl.teamx.service.UserService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +26,6 @@ import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 
-
 //import org.h2.store.PageInputStream;
 
 @Controller
@@ -38,31 +39,29 @@ public class TeamXController {
 	private ImageStorageService imageStorageService;
 
 	@GetMapping("/user")
-	public String getUserPage(Model model, HttpSession session){
+	public String getUserPage(Model model, HttpSession session) {
 		User user = (User) session.getAttribute("user");
-		if(user==null){
+		if (user == null) {
 			return "redirect:/";
 		}
-		//Refreshing user informations
+		// Refreshing user informations
 		model.addAttribute("user", userService.getUser(user.getId()));
 		return "user";
 	}
 
-	@PostMapping(value = "/user", params = {"editDescription"})
-	public String updateDescription(@ModelAttribute("user") User user,
-									Model model, HttpSession httpSession){
+	@PostMapping(value = "/user", params = { "editDescription" })
+	public String updateDescription(@ModelAttribute("user") User user, Model model, HttpSession httpSession) {
 		User to_persist_user = (User) httpSession.getAttribute("user");
 		to_persist_user.setDescription(user.getDescription());
 		userService.updateUser(to_persist_user);
-		httpSession.setAttribute("user",to_persist_user);
+		httpSession.setAttribute("user", to_persist_user);
 		return "redirect:/user";
 	}
 
-	
-	/** Trova  user con userId. */
+	/** Trova user con userId. */
 	@GetMapping("/users/{userId}")
 	public String getUser(Model model, @PathVariable(name = "userId") String userId) {
-		if(userId==null || userService.getUser(userId)==null)
+		if (userId == null || userService.getUser(userId) == null)
 			return "redirect:/";
 		User user = userService.getUser(userId);
 		model.addAttribute("user", user);
@@ -72,17 +71,16 @@ public class TeamXController {
 	/** restituisce tutti i team */
 	@GetMapping("/teams")
 	public String getTeams(Model model, HttpSession session) {
-		if(session.getAttribute("user")==null){
+		if (session.getAttribute("user") == null) {
 			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 			userService.createUser(user);
-			session.setAttribute("user",user);
+			session.setAttribute("user", user);
 		}
 
 		List<Team> teams = teamService.getAllTeams();
 		model.addAttribute("teams", teams);
 		return "teams";
 	}
-
 
 	/** Trova il team con teamId. */
 	@GetMapping("/teams/{teamId}")
@@ -92,7 +90,6 @@ public class TeamXController {
 		return "team";
 	}
 
-	
 	/** Crea un nuovo team (form). */
 	@GetMapping(value = "/teams", params = { "add" })
 	public String getTeamForm(Model model) {
@@ -103,22 +100,21 @@ public class TeamXController {
 
 	/** Crea un nuovo team. */
 	@PostMapping("/teams")
-	public String addTeam(HttpSession session, Model model, @ModelAttribute("form") @Valid AddTeamForm form, BindingResult bindingResult,
-			@RequestParam("file") MultipartFile file) {
+	public String addTeam(HttpSession session, Model model, @ModelAttribute("form") @Valid AddTeamForm form,
+			BindingResult bindingResult, @RequestParam("file") MultipartFile file) {
 
 		User user = (User) session.getAttribute("user");
 
 		if (!bindingResult.hasErrors()) {
 			String urlImage = null;
-			if(!file.isEmpty()) {
-				urlImage = imageStorageService.storeImage(file,
-						user.getName().toLowerCase()
-								+ form.getName().toLowerCase()+LocalDate.now().toString().toLowerCase());
+			if (!file.isEmpty()) {
+				urlImage = imageStorageService.storeImage(file, user.getName().toLowerCase()
+						+ form.getName().toLowerCase() + LocalDate.now().toString().toLowerCase());
 			}
-			Team team = teamService.createTeam(user, form.getName(),
-					form.getDescription(),form.getLocation(), urlImage);
+			Team team = teamService.createTeam(user, form.getName(), form.getDescription(), form.getLocation(),
+					urlImage);
 			model.addAttribute("user", user);
-			return "redirect:/teams/"+team.getId().toString();
+			return "redirect:/teams/" + team.getId().toString();
 		} else
 			return "addTeamForm";
 	}
@@ -161,57 +157,74 @@ public class TeamXController {
 	public String joinTeam(Model model, @PathVariable Long teamId) throws Exception {
 		Team team = teamService.getTeam(teamId);
 		String message = "da modificare";
-		//TODO: send email
-		//User u = new User("da", "cambiare", "non so come");
-		//userService.sendEmail(u, u, message);
+		// TODO: send email
+		// User u = new User("da", "cambiare", "non so come");
+		// userService.sendEmail(u, u, message);
 		model.addAttribute("team", team);
 		model.addAttribute("message", "richiesta inviata con successo");
 		return "team";
 	}
 
-	
-
 	@ExceptionHandler(MultipartException.class)
 	@ResponseBody
 	public ModelAndView handleFileException(HttpServletRequest request, Throwable ex) {
-	    //return your json insted this string.
+		// return your json insted this string.
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("url", request.getRequestURL());
-	    //mav.setViewName("errorPage");
-	    return mav;
-	  }
+		// mav.setViewName("errorPage");
+		return mav;
+	}
 
-	  @GetMapping("/error")
-	  public String renderErrorPage(HttpServletRequest httpRequest,Model model) {
-		   
-		  String errorMsg = "";
-		  int httpErrorCode = getErrorCode(httpRequest);
-   
-		  switch (httpErrorCode) {
-			  case 400: {
-				  errorMsg = "Http Error Code: 400. Bad Request";
-				  break;
-			  }
-			  case 401: {
-				  errorMsg = "Http Error Code: 401. Unauthorized";
-				  break;
-			  }
-			  case 404: {
-				  errorMsg = "Http Error Code: 404. Resource not found";
-				  break;
-			  }
-			  case 500: {
-				  errorMsg = "Http Error Code: 500. Internal Server Error";
-				  break;
-			  }
-		  }
-		  model.addAttribute("errorMsg", errorMsg);
-		  return "errorPage";
-	  }
-	   
-	  private int getErrorCode(HttpServletRequest httpRequest) {
-		  return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
-	  }
-  
+
+	@GetMapping("/error")
+	public String handleError(HttpServletRequest request,Model model) {
+		System.out.println("ciao core0");
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		System.out.println("ciao core");
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				model.addAttribute("errorMsg", new Integer(404));
+				return "error";
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				model.addAttribute("errorMsg", new Integer(500));
+				return "error";
+			}
+		}
+		model.addAttribute("errorMsg", new Integer(400));
+		return "error";
+	}
+/*	@GetMapping("/error")
+	public String renderErrorPage(HttpServletRequest httpRequest, Model model) {
+		System.out.println("ciao core");
+		String errorMsg = "";
+		int httpErrorCode = getErrorCode(httpRequest);
+
+		switch (httpErrorCode) {
+		case 400: {
+			errorMsg = "Http Error Code: 400. Bad Request";
+			break;
+		}
+		case 401: {
+			errorMsg = "Http Error Code: 401. Unauthorized";
+			break;
+		}
+		case 404: {
+			errorMsg = "Http Error Code: 404. Resource not found";
+			break;
+		}
+		case 500: {
+			errorMsg = "Http Error Code: 500. Internal Server Error";
+			break;
+		}
+		}
+		model.addAttribute("errorMsg", errorMsg);
+		return "errorPage";
+	}
+
+	private int getErrorCode(HttpServletRequest httpRequest) {
+		return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+	}*/
 
 }
